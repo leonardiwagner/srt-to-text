49e0b6ce0b19a8e98b5dbbf010624f9b60028c25
Wagner Leonardi <leonardiwagner@gmail.com>
2020-08-11 18:38:42 -0300

    update upload details

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject srt-to-text "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Parse subtitle files"
+  :url "https://github.com/leonardiwagner/srt-to-text"
+  :license {:name "Apache License"
+            :url  "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
                  [org.clojure/clojure "1.10.1"]
                  [org.apache.tika/tika-parsers "1.24.1"]]
